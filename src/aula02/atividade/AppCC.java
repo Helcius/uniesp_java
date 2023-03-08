babb54e986e804fcd0c368514cf28aa44569ddc4
Maria Gabriela <112910372+gabrielaanselmo@users.noreply.github.com>
2023-03-07 23:24:29 -0300

    commit
    commit

--- src/aula02/atividade/AppCC.java
+++ src/aula02/atividade/AppCC.java
@@ -1,12 +1,16 @@
 public class AppCC {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o nome do titular da conta: ");
+        String titular = scanner.nextLine();
+        ContaCorrente conta = new ContaCorrente(titular);
 
-ContaCorrente conta = new ContaCorrente();
-conta.setNome("Maria Gabriela");
-conta.depositar(100.0);
-conta.sacar(50.0);
-System.out.println("Titular da conta: " + conta.getNome());
-System.out.println("Saldo da conta: " + conta.getSaldo());
+        System.out.print("Informe o valor a ser depositado: ");
+        double valorDeposito = scanner.nextDouble();
+        conta.depositar(valorDeposito);
 
+        System.out.print("Informe o valor a ser sacado: ");
+        double valorSaque = scanner.nextDouble();
+        conta.sacar(valorSaque);
+    }
 }
