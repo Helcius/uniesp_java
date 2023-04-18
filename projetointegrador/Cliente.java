a0b19f52ac4f67963100cf2fab343199b179bac7
Gabriela <gabrielaanselmao@gmail.com>
2023-04-18 19:19:50 -0300

    commit
    commit

--- projetointegrador/Cliente.java
+++ projetointegrador/Cliente.java
@@ -26,9 +26,9 @@ public class Cliente {
     }
 
 
-    //Objeto cliente tem acesso ao método investir, que recebe um valor que vai ser retirado do seu saldo, e
-    //recebe também um valor em meses. Na conclusão com sucesso do método ele recebe uma mensagem com o valor
-    //que investiu e o valor que teve de retorno, o qual é adicionado ao saldo.
+    // objeto cliente tem acesso ao método investir, que recebe um valor que vai ser retirado do seu saldo, e
+    // recebe também um valor em meses. na conclusão com sucesso do método ele recebe uma mensagem com o valor
+    // que investiu e o valor que teve de retorno, o qual é adicionado ao saldo.
     public void investir(double valorInvestido, int meses) {
 
         //checa se o cliente tem saldo para efetuar o investimento, caso não, envia uma mensagem, mantém o
@@ -40,19 +40,19 @@ public class Cliente {
             return;
         }
 
-        //Valor arbitrário de juros, corresponde a 10% ao mês
+        // valor arbitrário de juros, corresponde a 10% ao mês
         double juros = 10;
 
-        //Variável recebe a fórmula para juros composto
+        // variável recebe a fórmula para juros composto
         double valorAcumulado = valorInvestido * (Math.pow((1 + juros / 100), meses));
         valorAcumulado = Math.round(valorAcumulado * 100.0) / 100.0;
 
 
-        //Mensagens para informar os valores da movimentação
+        // mensagens para informar os valores da movimentação
         System.out.println("O cliente de CPF " + this.cpf + " fez um investimento de R$" + valorInvestido + ".");
         System.out.println("Após " + meses + " mês(es), o investimento resultou em uma quantia de R$" + valorAcumulado + ".");
 
-        //Soma o valor acumulado ao saldo do cliente
+        // soma o valor acumulado ao saldo do cliente
         this.saldo += valorAcumulado;
         System.out.println("O saldo atual do cliente é de R$" + this.saldo);
 
